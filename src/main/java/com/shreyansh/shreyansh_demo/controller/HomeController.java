40c7fcbd2d881d8c2bc6bc20e19f113023ad4581
Shreyansh Tiwari <Shreyansh.11906963@lpu.in>
2025-03-17 20:35:12 +0530

    route parameters(path variables)

--- src/main/java/com/shreyansh/shreyansh_demo/controller/HomeController.java
+++ src/main/java/com/shreyansh/shreyansh_demo/controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.shreyansh.shreyansh_demo.controller;
 
 import com.shreyansh.shreyansh_demo.model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //adding annotations so that my class will behave as controller
 //2nd annotation is to tell that this class will return a data in a response body
@@ -24,5 +22,12 @@ public class HomeController {
         user.setId("98678");
         user.setEmailID("fghj@Fghj.jhg");
         return user;
+    }
+
+//    using route parameters
+    @PostMapping("/user/{id1}/{id2}")
+    public String getPathVariables(@PathVariable String id1, @PathVariable("id2") String name){
+        // here we explicitly mapped name with id2
+        return "Path variables were: "+id1+" & "+name;
     }
 }
