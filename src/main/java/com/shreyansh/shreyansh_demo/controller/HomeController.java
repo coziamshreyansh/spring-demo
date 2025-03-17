f2948307167c3e618efe649c25b34394a3a400e9
Shreyansh Tiwari <Shreyansh.11906963@lpu.in>
2025-03-17 19:27:50 +0530

    basic rest api

--- src/main/java/com/shreyansh/shreyansh_demo/controller/HomeController.java
+++ src/main/java/com/shreyansh/shreyansh_demo/controller/HomeController.java
@@ -1,17 +1,28 @@
 package com.shreyansh.shreyansh_demo.controller;
 
-import org.springframework.stereotype.Controller;
+import com.shreyansh.shreyansh_demo.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 //adding annotations so that my class will behave as controller
 //2nd annotation is to tell that this class will return a data in a response body
-@Controller
-@ResponseBody
+
+// instead of using both annotations we can use one this will tell class to behave like restful service controller
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String home(){
         return "Hello Shreyansh!";
+    }
+
+    @RequestMapping( value = "/user", method = RequestMethod.POST)
+    public User getUserInstance(){
+        User user = new User();
+        user.setUsername("shanxTumHanTum");
+        user.setId("98678");
+        user.setEmailID("fghj@Fghj.jhg");
+        return user;
     }
 }
---
+++ src/main/java/com/shreyansh/shreyansh_demo/model/User.java
@@ -0,0 +1,31 @@
+package com.shreyansh.shreyansh_demo.model;
+
+public class User {
+    private String id;
+    private String username;
+    private String emailID;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+}
